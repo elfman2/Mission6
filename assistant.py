cff6c0bf18e4bdb08ff643a05a908b192ddb445e
Skudle <thouny.thea@student.uclouvain.be>
2022-10-27 19:47:08 +0200

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -68,36 +68,46 @@ user_input  = input("Choississez une commande. Pour voir toutes les commandes di
 
 def help():
     if user_input == 'help':
-        print("file <name>: spécifie le nom d'un fichier sur lequel l'outil doit travailler à partir de ce moment")
-        print("info: montre le nombre de lignes et de caractères du fichier")
-        print("file <name>: spécifie le nom d'un fichier sur lequel l'outil doit travailler à partir de ce moment")
-        print("dictionary: utilise le fichier comme dictionnaire à partir de maintenant")
-        print("search <word>: détermine si le mot est dans le dictionnaire")
-        print("sum <number1> ... <numbern>: calcule la somme des nombres spécifiés")
-        print("avg <number1> ... <numbern>: calcule la moyenne des nombres spécifiés")
-        print("help: montre des instructions à l'utilisateur")
-        print("exit: arrête l'outil")
+        print("""("file <name>: spécifie le nom d'un fichier sur lequel l'outil doit travailler à partir de ce moment")
+        ("info: montre le nombre de lignes et de caractères du fichier")
+        ("file <name>: spécifie le nom d'un fichier sur lequel l'outil doit travailler à partir de ce moment")
+        ("dictionary: utilise le fichier comme dictionnaire à partir de maintenant")
+        ("search <word>: détermine si le mot est dans le dictionnaire")
+        ("sum <number1> ... <numbern>: calcule la somme des nombres spécifiés")
+        ("avg <number1> ... <numbern>: calcule la moyenne des nombres spécifiés")
+        ("help: montre des instructions à l'utilisateur")
+        ("exit: arrête l'outil")""")
+        
+    
 
-def sum(num):
+def sum():
     if user_input == "sum":
         run2 = True
         total = 0
-        
         num2 = 0
         while run2:
-            user_number = int(input("Entrez un numéro"))
-            num = user_number
-            user_number2 = int(input("Entrez un second numéro"))
-            num2 = user_number2
+            user_number = int(input("Entrez un numéro "))
+            user_number2 = int(input("Entrez un numéro "))
+            total = total + user_number + user_number2
             ask = input("Voulez-vous continuez? Entrez 'oui' si vous souhaitez continuer, sinon entrez 'non'")
-            total = total + num + num2
-            '''if ask == "oui": 
-            #elif ask == "non":
-                total = total + num + num2
+            yes = True
+            if ask == "oui":
+                while yes:
+                    user_number3 = int(input("Entrez un numéro "))
+                    total += user_number3
+                    ask2 = input("Si vous voulez continuer, entrez oui. Sinon entrez non")
+                    if ask2 == 'yes':
+                        continue
+                    elif ask2 == 'non':
+                        print(total)
+                        yes = False
+                        run2 = False
+                break
+                
+            elif ask == "non":
                 print(total)
-                run2 = False'''
-
+                run2 = False
 
 
 help()
-sum(0)
+sum()
