86702f5ae0fc7f18d1161793c0f7040d9bf796c4
Skudle <113626842+Skudle@users.noreply.github.com>
2022-10-29 11:48:26 +0200

    Presque terminé
    Manque plus que le 'exit'

--- assistant.py
+++ assistant.py
@@ -1,22 +1,50 @@
 import sys # systeme, permet acceder a l'entree du clavier et a la sortie sur ecran
-from optparse import OptionParser # 
+import ast
 
+file_name = None
+dico = None
+
+def file1(args):
+    global file_name
+    
+    try:
+        if len(args) != 1:
+            return 'Entrez un argument correct '
+        else:
+            file_name = open(args[0], 'r')
+            return 'Fichier défini comme fichier de travail '
+    except:
+        return f"File {args} not found"
+        
+    
 
 def prompt():
     sys.stdout.write('>')
     sys.stdout.flush()
 
-def dictionary(filename):
-    dictionary2 = {}
-    with open(filename, 'r') as f:
-        for line in f:
-            key, value = line.split(',')
-            dictionary2[key] = value
-        return dictionary2
+def dictionary(args):
+    global dico
+    dico = args
+    
+    try:
+        if len(args) != 1:
+            return 'Entrez un argument correct '
+        else:
+            file_name = open(args[0], 'r')
+            return 'Fichier défini comme dictionnaire '
+    except:
+        return f"File {args} not found"
+
 
 def CLI_interactive():
+    prompt()
     for line in sys.stdin:
+        if line == 'exit':
+            break
+    else:
+        prompt()
         yield line
+        
 def help():
     
     """
@@ -40,7 +68,7 @@ Entrez des numéros.
 Note: Après chaque numéro, faites espace au lieu de 'entrer' pour additionner les nombres.
 Faites 'Entrer' pour afficher le resulat.""" ))
     tableau = user_input.split()
-    convert = [float(i) for i in tableau]
+    convert = [ast.literal_eval(i) for i in tableau]
     x = sum(convert)
     print(x)  
         
@@ -51,9 +79,24 @@ Entrez des numéros.
 Note: Après chaque numéro, faites espace au lieu de 'entrer' pour additionner les nombres.
 Faites 'Entrer' pour trouver la moyenne et afficher le resulat.""" ))
     tableau = user_input.split()
-    convert = [float(i) for i in tableau]
+    convert = [ast.literal_eval(i) for i in tableau]
     x = sum(convert)
     print(x/(len(convert)))
+    
+def search(args):
+    
+    dictionary2 = {}
+    with open(dico, 'r') as f:
+        for line in f:
+            key, value = line.split(',')
+            dictionary2[key] = value
+    
+    if args in dictionary2:
+        return f"{args} est dans le dictionnaire"
+    else:
+        return f"{args} n'est pas dans le dictionnaire"
+        
+    
     
 CLI_interactive()
 
