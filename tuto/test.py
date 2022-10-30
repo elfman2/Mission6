1e397405f7320754d9d1be451fbe8c8cd1bf943b
elfman2 <nicolas.valot@gmail.com>
2022-10-30 09:14:47 +0100

    test sum

--- tuto/test.py
+++ tuto/test.py
@@ -2,6 +2,8 @@ import unittest
 from unittest.mock import patch
 from io import StringIO
 import assistant as a
+# ref https://ryip.me/posts/python/unittest-stdout-stderr/
+
 class TestCLI(unittest.TestCase):
 
     @patch('sys.stdout', new_callable = StringIO)
@@ -10,6 +12,10 @@ class TestCLI(unittest.TestCase):
        expected_out = '> '
        self.assertEqual(stdout.getvalue(),expected_out)
 
+    def test_sum(self):
+      out=a.sum_command(['1', '2','3'])
+      index= out.find('6')
+      self.assertGreater(index,0)
 
 if __name__ == '__main__':
     unittest.main()
